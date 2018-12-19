33eee6d670e335a90af66573de5ef64d2a1e0f18
ZhouYuhan <flyzyh@hotmail.com>
2018-12-19 23:47:29 +0800

    finished List Test in LiaoXueFeng's Website

--- test1/t2.py
+++ test1/t2.py
@@ -1,2 +1,25 @@
-print(r'''hellooo
-wooorld''')
+classmates = ['Michael', 'Bob', 'Tracy']
+print(classmates)
+print(len(classmates))
+print(classmates[0])
+print(classmates[1])
+print(classmates[2])
+print(classmates[-1])
+classmates.insert(1, 'Jack')
+print('===========after insert============')
+print(classmates)
+print(len(classmates))
+classmates.append('Adam')
+print('===========after append============')
+print(classmates)
+print(len(classmates))
+
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+
+print(L[0][0])
+print(L[1][1])
+print(L[2][2])
