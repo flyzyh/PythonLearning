f72e347db1937bf47ef83d74a146ca98bb23cf84
flyzyh <flyzyh@hotmail.com>
2018-12-20 23:16:29 +0800

    finished python if test

--- test1/t3.py
+++ test1/t3.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 age = 9
 if age >= 18:
     print('your age is', age)
@@ -8,3 +9,16 @@ elif age >= 6:
 else:
     print('your age is', age)
     print('kid')
+
+height = 1.75
+weight = 80.5
+bmi = weight / (height * height)
+print('bmi is ',bmi)
+if bmi < 18.5:
+    print('体重过轻')
+elif bmi < 25:
+    print('体重正常')
+elif bmi < 28:
+    print('体重肥胖')
+elif bmi < 32:
+    print('严重肥胖')
